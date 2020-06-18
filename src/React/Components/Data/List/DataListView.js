10c6c5611e4eb913ad0d92574813a439eb306e67
Cody Swann <cody@gunnertech.com>
2020-06-18 16:36:52 -0400

    version bump

--- src/React/Components/Data/List/DataListView.js
+++ src/React/Components/Data/List/DataListView.js
@@ -5,46 +5,46 @@ import uniqByProp from '../../../../Util/uniqByProp';
 
 
 export default ({loading, hasMoreItems, objects, onEndReached, RenderComponent, viewVariables, clientFilter = item => !!item, clientSort}) => {
-  // const handleEndReached = () => {
-  //   if ((window.innerHeight + window.scrollY - 50) >= document.body.offsetHeight) {
-  //     console.log("you're at the bottom of the page", !!hasMoreItems);
-  //     !!hasMoreItems && onEndReached()
-  //   }
-  // }
+  const handleEndReached = () => {
+    if ((window.innerHeight + window.scrollY - 50) >= document.body.offsetHeight) {
+      console.log("you're at the bottom of the page", !!hasMoreItems);
+      !!hasMoreItems && onEndReached()
+    }
+  }
 
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleEndReached);
+  useEffect(() => {
+    window.addEventListener('scroll', handleEndReached);
 
-  //   return () => window.removeEventListener('scroll', handleEndReached)
-  // })
+    return () => window.removeEventListener('scroll', handleEndReached)
+  })
   
   return (
-  //   objects.filter(clientFilter).map(item => 
-  //     <RenderComponent
-  //       key={item.id}
-  //       id={item.id}
-  //       object={item}
-  //       filter={clientFilter}
-  //       {...viewVariables ?? {}}
-  //     />  
-  //   )
-    !!loading ? "Loading..." :
-    <FlatList
-      hasMoreItems={hasMoreItems}
-      list={
-        objects.filter(uniqByProp('id')).slice().sort(clientSort).filter(clientFilter)
-      }
-      loadMoreItems={onEndReached}
-      renderItem={item => 
-        <RenderComponent
-          key={item.id}
-          id={item.id}
-          object={item}
-          filter={clientFilter}
-          {...viewVariables ?? {}}
-        />
-      }
-    />
+    objects.filter(clientFilter).map(item => 
+      <RenderComponent
+        key={item.id}
+        id={item.id}
+        object={item}
+        filter={clientFilter}
+        {...viewVariables ?? {}}
+      />  
+    )
+    // !!loading ? "Loading..." :
+    // <FlatList
+    //   hasMoreItems={hasMoreItems}
+    //   list={
+    //     objects.filter(uniqByProp('id')).slice().sort(clientSort).filter(clientFilter)
+    //   }
+    //   loadMoreItems={onEndReached}
+    //   renderItem={item => 
+    //     <RenderComponent
+    //       key={item.id}
+    //       id={item.id}
+    //       object={item}
+    //       filter={clientFilter}
+    //       {...viewVariables ?? {}}
+    //     />
+    //   }
+    // />
   )
 }
   
