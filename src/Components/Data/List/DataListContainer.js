b3cdf122f183051391d468737434ff3e9bbb3746
Cody Swann <cody@gunnertech.com>
2020-08-12 11:57:34 -0400

    version bump

--- src/Components/Data/List/DataListContainer.js
+++ src/Components/Data/List/DataListContainer.js
@@ -89,5 +89,5 @@ export default ({
       usedButton={usedButton}
       forceMore={forceMore}
     />
-  , [memoizedObjects])//, loading, handleEndReached, handleRefresh, onCreateLoading, onUpdateLoading, clientFilter, useButton, forceMore])
+  , [memoizedObjects, handleEndReached, handleRefresh, onCreateLoading, onUpdateLoading, clientFilter, useButton, forceMore])//, loading, handleEndReached, handleRefresh, onCreateLoading, onUpdateLoading, clientFilter, useButton, forceMore])
 }
